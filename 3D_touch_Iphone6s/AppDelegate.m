c07e28e7739d025df05074c6f7ea67c29ef472c5
yachaocn <zyachao@163.com>
2015-12-11 11:25:48 +0800

    完成3DTouch_peek_pop

--- 3D_touch_Iphone6s/AppDelegate.m
+++ 3D_touch_Iphone6s/AppDelegate.m
@@ -16,9 +16,45 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+//    动态创建一个shotrcutItem
+    [self createDynamicShortcutItem];
+    if (launchOptions[UIApplicationLaunchOptionsSourceApplicationKey] != nil) {
+        return NO;//shortcutItem 不为空证明要加载shortcutitem，不启动应用程序。
+    }else{
+        return YES;
+    }
+    
     return YES;
 }
+-(void)createDynamicShortcutItem
+{
+    //动态创建UIApplicationShortcutItem
+    
+    //1. 创建一个图标的icon
+    UIApplicationShortcutIcon *icon = [UIApplicationShortcutIcon iconWithTemplateImageName:@"imageName"];
+    //2.创建一个系统类型字符串
+    NSString *itemType = [NSString stringWithFormat:@"%ld",(long)UIApplicationShortcutIconTypeSearch];
+    //3.创建一个item对象
+    UIApplicationShortcutItem *item = [[UIApplicationShortcutItem alloc]initWithType:itemType localizedTitle:@"搜索" localizedSubtitle:@"可以自定义搜索哦" icon:icon userInfo:nil];
+    UIApplication *application = [UIApplication sharedApplication];
+    NSArray *array = application.shortcutItems;
+    NSMutableArray *mutShortItems = [NSMutableArray arrayWithArray:array];
+    [mutShortItems addObject:item];
+    application.shortcutItems = mutShortItems;
+    
+}
+- (void)application:(UIApplication *)application performActionForShortcutItem:(nonnull UIApplicationShortcutItem *)shortcutItem completionHandler:(nonnull void (^)(BOOL))completionHandler
+{
+    switch ([shortcutItem.type integerValue]) {
+        case 5:
+            NSLog(@"selected UIApplicationShortcutIconTypeSearch ");
+            break;
+            
+        default:
+            break;
+    }
+}
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- 3D_touch_Iphone6s/ViewController.m
+++ 3D_touch_Iphone6s/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIViewControllerPreviewingDelegate>
 
 @end
 
@@ -16,9 +16,64 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    if (self.traitCollection.forceTouchCapability == UIForceTouchCapabilityAvailable) {
+        NSLog(@"3dtouch availabe");
+    }else{
+        NSLog(@"3dtouch not availabe");
+    }
+//    注册一个peek 和 pop 控制器使其支持peek and pop，并使其遵守UIViewControllerPreviewingDelegate代理协议
+    [self registerForPreviewingWithDelegate:self sourceView:self.view];
+    
+//动态创建UIApplicationShortcutItem
+    
+    //1. 创建一个图标的icon
+    UIApplicationShortcutIcon *icon = [UIApplicationShortcutIcon iconWithTemplateImageName:@"imageName"];
+    //2.创建一个系统类型字符串
+    NSString *itemType = [NSString stringWithFormat:@"%ld",(long)UIApplicationShortcutIconTypeSearch];
+    //3.创建一个item对象
+    UIApplicationShortcutItem *item = [[UIApplicationShortcutItem alloc]initWithType:itemType localizedTitle:@"搜索" localizedSubtitle:@"可以自定义搜索哦" icon:icon userInfo:nil];
+    UIApplication *application = [UIApplication sharedApplication];
+    NSArray *array = application.shortcutItems;
+    NSMutableArray *mutShortItems = [NSMutableArray arrayWithArray:array];
+    [mutShortItems addObject:item];
+    application.shortcutItems = mutShortItems;
+    
+//修改静态创建的shortcutitem对象
+    NSArray *shortCutItems = [UIApplication sharedApplication].shortcutItems;
+    
+    UIApplicationShortcutItem *item0 = [shortCutItems objectAtIndex:0];
+    UIMutableApplicationShortcutItem *mutItem0 = [item0 mutableCopy];
+    mutItem0.localizedTitle = @"动态修改的title";
+    
+    NSMutableArray *mutShortCutItems = [NSMutableArray arrayWithArray:shortCutItems];
+    [mutShortCutItems replaceObjectAtIndex:0 withObject:mutItem0];
+    [UIApplication sharedApplication].shortcutItems = mutShortCutItems;
+    
+ 
 }
-
+#pragma mark - peek and pop
+//1.注册要previewing 的控制器
+//2.实现UIViewControllerPreviewingDelegate代理方法
+- (nullable UIViewController *)previewingContext:(id <UIViewControllerPreviewing>)previewingContext viewControllerForLocation:(CGPoint)location NS_AVAILABLE_IOS(9_0)
+{
+//    peek
+    UIViewController *childVC = [[UIViewController alloc] init];
+    childVC.view.backgroundColor = [UIColor yellowColor];
+    childVC.preferredContentSize = CGSizeMake(0.0,300);
+    
+    CGRect rect = CGRectMake(10, location.y - 10, self.view.frame.size.width - 20,20);
+    previewingContext.sourceRect = rect;
+    return childVC;
+    
+    
+}
+- (void)previewingContext:(id <UIViewControllerPreviewing>)previewingContext commitViewController:(UIViewController *)viewControllerToCommit NS_AVAILABLE_IOS(9_0)
+{
+//    pop
+    [self showViewController:viewControllerToCommit sender:self];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
